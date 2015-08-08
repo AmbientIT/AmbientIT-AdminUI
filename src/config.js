04f8acffe063b27e040d8efef8d40d6028d81b40
charl <charles.jacquin@ambient-it.org>
2015-08-08 03:59:06 +0200

    strict di

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
 export default
 /* @ngInject */
 ($locationProvider, $compileProvider)=> {
-  $locationProvider.html5Mode({
-    enabled: false,
-    requireBase:true
-  });
+  $locationProvider
+    .html5Mode({
+      enabled: false,
+      requireBase:true
+    })
+    .hashPrefix('!');
 
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob|localhost):|data:image\//);
 
